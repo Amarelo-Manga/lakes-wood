711d69df61c9208b0ff31401923716195a68b176
Elvis B. Martins <elvisbmartins@gmail.com>
2018-01-17 16:21:50 -0200

    template page banner

---
+++ templates/page-banner.php
@@ -0,0 +1,42 @@
+<?php
+	$args = array(
+		        'posts_per_page' => -1,
+		        'post_type' => 'banner'
+       	 	);
+    $loop_banner = new WP_Query($args);
+?>
+
+<div id="banner" class="carousel slide" data-ride="carousel">
+  	<div class="carousel-inner content" role="listbox">
+		<?php 
+			$count = 1;
+		    while ($loop_banner->have_posts()) : $loop_banner->the_post();
+		    	if( $count == 1){
+		    		$active = "active";
+		    	}else{
+		    		$active = "";
+		    	}
+		?>    	
+		<div class="carousel-item <?php echo $active; ?>" style="background:url('<?php the_post_thumbnail_url(); ?>');
+    background-size: contain;background-repeat: no-repeat;" >
+	      	<div class="container">
+		      	<div class="row">
+		 			<div class="titulo col-lg-2 col-md-2 col-sm-12"><?php the_title(); ?></div>
+		 			<div class="texto col-lg-5 col-md-5 col-sm-12"><?php the_content(); ?></div>		
+		      	</div>
+	      	</div>
+	    </div>
+		<?php 
+				$count++;
+		    endwhile;
+		?>
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		</a>
+	</div>
+</div>
